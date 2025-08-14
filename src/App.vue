5c7504377fb6d27b4a8aa6b0ee1659df0ade9d7d
IngridKao <ingrid-kao@qware.com>
2025-08-14 11:01:28 +0800

    做完任務1,2,3,4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 // 任務1. 引入FavList組件到App.vue的aside中
+import FavList from '@/components/FavList.vue'
 
 // 如果使用pinia
 // import useFavoriteStore from '@/stores/favorites'
@@ -10,20 +11,37 @@ import { ref, onMounted } from 'vue'
 // 目前畫面中僅呈現defaultData
 // 請將資料替換成`public/albums.json`中的專輯資料
 // （提示：想一下怎麼把資料帶進來？）
-const defaultData = {
-  "id": 1,
-  "images": "https://i.scdn.co/image/ab67616d00001e023e59f3e73b99ed248ab7bae2",
-  "name": "Day & Night (feat. Jay Park)",
-  "artists": "Lee Young Ji"
-}
-onMounted(()=>{})
+// const defaultData = {
+//   "id": 1,
+//   "images": "https://i.scdn.co/image/ab67616d00001e023e59f3e73b99ed248ab7bae2",
+//   "name": "Day & Night (feat. Jay Park)",
+//   "artists": "Lee Young Ji"
+// }
+const albums = ref([])
+onMounted(()=>{
+  fetch('src/assets/albums.json')
+  .then(data => data.json())
+  .then(res=>{
+    albums.value = res
+  })
+})
 
 // 任務3:開啟關閉側拉選單(收藏列表)
 const asideToggle = ref(false)
-const toggleAside = () => {}
+const toggleAside = () => {
+  asideToggle.value = !asideToggle.value
+}
 
 // 任務4.專輯資料可以被input搜尋
 const search = ref('')
+const display = computed(()=>{
+  if(search.value === '') return albums.value
+  // 皆轉成小寫
+  const searchStr = search.value.toLowerCase()
+  return albums.value.filter(item=>{
+    return item.name.toLowerCase().includes(searchStr) || item.artists.toLowerCase().includes(searchStr)
+  })
+})
 
 // 任務5.加入我的收藏
 // 不限定方式，如果不知道怎麼使用pinia可以用其他方式
@@ -42,21 +60,23 @@ const addFav = (item) => {
   </header>
 
   <main>
-    <div class="card">
-      <img :src="defaultData.images" />
+    <div class="card" v-for="item in display" :key="item.id">
+      <img :src="item.images" />
       <div class="card_body">
-        <h6>{{ defaultData.name }}</h6>
-        <p>{{ defaultData.artists }}</p>
+        <h6>{{ item.name }}</h6>
+        <p>{{ item.artists }}</p>
       </div>
       <div class="card_footer">
-        <button class="favoriteBtn" @click="addFav(defaultData)">
+        <button class="favoriteBtn" @click="addFav(item)">
           <img src="~@/assets/heartBlack.png" alt="收藏專輯" />
         </button>
       </div>
     </div>
   </main>
 
-  <aside :class="{ open: asideToggle }"></aside>
+  <aside :class="{ open: asideToggle }">
+    <FavList />
+  </aside>
 </template>
 
 <style lang="scss" scoped>
@@ -67,6 +87,9 @@ header ,main{
 header {
   justify-content: space-between;
   margin-bottom: 0.5rem;
+  button{
+    z-index: 2;
+  }
 }
 main {
   flex-wrap: wrap;
@@ -76,6 +99,7 @@ aside {
   width: 20rem;
   height: 100vh;
   position: fixed;
+  z-index: 1;
   top: 0;
   right: -20rem;
 
